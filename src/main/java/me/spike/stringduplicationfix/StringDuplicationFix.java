cad1c064d5656e2073485ce07d46f4a048a7819b
Spike <101163288+Sinabrowler@users.noreply.github.com>
2022-07-07 15:23:51 +0430

    Update StringDuplicationFix.java

--- src/main/java/me/spike/stringduplicationfix/StringDuplicationFix.java
+++ src/main/java/me/spike/stringduplicationfix/StringDuplicationFix.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StringDuplicationFix extends JavaPlugin {
 
+
     private static StringDuplicationFix instance;
+
+
     public static StringDuplicationFix getInstance(){
+
         return instance;
     }
+
     @Override
     public void onEnable() {
-        instance = this;
-        getServer().getPluginManager().registerEvents(new AntiDup(), this);
 
+        instance = this;
+
+        getServer().getPluginManager().registerEvents(new AntiDup(), this);
     }
+
 }
